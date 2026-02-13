d6c0e4b4aaf6cb88c6894f8a94e6519eaf4bb030
AJAY VEDULA <ajaykumarvedula0501@gmail.com>
2026-02-13 17:25:29 -0600

    Project Created

---
+++ CG_Merchandise_Mart/CG_Merchandise_MartApp.swift
@@ -0,0 +1,17 @@
+//
+//  CG_Merchandise_MartApp.swift
+//  CG_Merchandise_Mart
+//
+//  Created by VEDULA AJAY KUMAR on 2/13/26.
+//
+
+import SwiftUI
+
+@main
+struct CG_Merchandise_MartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CG_Merchandise_MartTests/CG_Merchandise_MartTests.swift
@@ -0,0 +1,16 @@
+//
+//  CG_Merchandise_MartTests.swift
+//  CG_Merchandise_MartTests
+//
+//  Created by VEDULA AJAY KUMAR on 2/13/26.
+//
+
+import Testing
+
+struct CG_Merchandise_MartTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
